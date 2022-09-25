e652d8dd61ba318ecb8db732b883004d7a79e102
tyson.alcorn <tyson.alcorn@landtraindesigns.com>
2022-09-24 20:41:02 -0600

    Update est3.test.js

--- test/est3.test.js
+++ test/est3.test.js
@@ -1,14 +1,2 @@
-const {ioArray} = require('./test_data/test-data.js');
-const {Devices} = require('../index.js');
 
-const config = {
-    restartLoopsAfterPanel: false, 
-    continuousAddressing: false, 
-    separateAddressing: false, 
-    facp: 'io' 
-}
-
-const deviceSetter = new Devices(ioArray, config);
-
-console.log(deviceSetter.init());
 
